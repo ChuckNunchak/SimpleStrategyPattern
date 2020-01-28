eca068c96d43e34f75658a6c2a3695733d4d3225
ChuckNunchak <54897002+ChuckNunchak@users.noreply.github.com>
2020-01-28 20:36:39 +0300

    Add files via upload

---
+++ MilliAttack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class MilliAttack : IAttack
+    {
+        public void doAttack()
+        {
+            Console.WriteLine("Milli attack");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            player.Attak(new MilliAttack());
+            player.Attak(new AttackOnRange());
+            Console.ReadKey();
+        }
+    }
+}
